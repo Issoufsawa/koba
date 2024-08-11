fff651a166d36be88f2ec02a7642e725b6b9e1ea
moussa <lelemoussa12@gmail.com>
2024-08-11 19:41:09 +0200

    modification des services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ServicesComponent } from './services/services.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { SingleservicesComponent } from './singleservices/singleservices.component';
 
 export const routes: Routes = [
     {path : 'services' , component : ServicesComponent},
-    {path : '' , component :AccueilComponent}
-    
+    {path : '' , component :AccueilComponent},
+    {path: 'singlesservices', component : SingleservicesComponent}
 ];
