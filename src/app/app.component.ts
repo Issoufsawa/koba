685665ee51e9e2b497c4d0c968d85c4ca77ae8a2
moussakante12 <moussa.kante@yoomi-solution.com>
2024-08-11 04:40:50 +0000

    correction des routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { AccueilComponent } from "./accueil/accueil.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -7,7 +7,7 @@ import { FooterComponent } from "./footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AccueilComponent, RouterLink, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, AccueilComponent, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
