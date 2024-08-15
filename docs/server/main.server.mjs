00bc3d0c20fae58a09a15c4413f2bea32973289c
moussakante12 <moussa.kante@yoomi-solution.com>
2024-08-15 16:33:55 +0000

    doc folder1

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-THFARZLW.mjs";import"./chunk-T35GAF24.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-XANSCVAI.mjs";import"./chunk-T35GAF24.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
