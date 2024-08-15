29a53bf97a0a80c39c0012e681489c1be5fd330b
moussakante12 <moussa.kante@yoomi-solution.com>
2024-08-15 14:26:12 +0000

    MOUSSA  merge 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
-=======
-import { RouterModule, RouterOutlet } from '@angular/router';
->>>>>>> issouf
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, HeaderComponent, AccueilComponent, RouterModule, FooterComponent],
-=======
+
   imports: [ HeaderComponent,RouterOutlet,FooterComponent],
->>>>>>> issouf
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
